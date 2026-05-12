d2d5ed4b4c56a76fc0548ce7cbafedce78b2079c
Riya Mhatre <riyamhatre2005@gmail.com>
2026-05-12 23:04:32 +0530

    Update drive_tool.py

--- backend/drive_tool.py
+++ backend/drive_tool.py
@@ -1,18 +1,23 @@
+import os
+import json
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
-SERVICE_ACCOUNT_FILE = 'credentials.json'
+google_creds = os.getenv("GOOGLE_CREDENTIALS_JSON")
 
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE,
+credentials_info = json.loads(google_creds)
+
+credentials = service_account.Credentials.from_service_account_info(
+    credentials_info,
     scopes=SCOPES
 )
 
 service = build('drive', 'v3', credentials=credentials)
 
 def search_drive(query):
+
     results = service.files().list(
         q=query,
         pageSize=10,
